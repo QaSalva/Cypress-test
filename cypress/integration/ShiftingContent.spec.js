0ce86b75ca139c0d001da45ff5d1b0a4f3ed61a9
Salva <salvadoralmvega@gmail.com>
2022-11-05 11:50:04 +0100

    Shifting Content Exercise

--- cypress/integration/ShiftingContent.spec.js
+++ cypress/integration/ShiftingContent.spec.js
@@ -4,10 +4,6 @@ it("Shifting Content Menu has the five elements", () =>{
     cy.visit("https://the-internet.herokuapp.com")
     cy.get(':nth-child(39) > a').click()
     cy.get('[href="/shifting_content/menu"]').click()
-    cy.contains('Home')
-    cy.contains('About')
-    cy.contains('Contact Us')
-    cy.contains('Portfolio')
-    cy.contains('Gallery')
-
+    cy.get('li').should('have.length' ,5 )
+  
 })
